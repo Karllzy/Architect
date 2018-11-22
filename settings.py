03bec7144d6b0e3a7e8ba3d6428900fa1f9c60dd
Karllzy <36060993+Karllzy@users.noreply.github.com>
2018-11-23 00:06:16 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 class Settings():
     """
     存储所有设置的类
@@ -9,4 +12,29 @@ class Settings():
         """
         self.screen_width = 1000
         self.screen_height = 618
-        self.bg_color = (255, 255, 255)
+        self.bg_color = (245, 245, 247)
+
+        self.GOLD = pygame.Color('gold')
+        self.RED = pygame.Color('red')
+        self.GREEN = pygame.Color('green')
+        self.BLUE = pygame.Color("blue")
+        self.GREY = pygame.Color('grey')
+        self.BLACK = pygame.Color('black')
+        self.PURPLE = pygame.Color('purple')
+
+        self.mouse_state = -1
+        # -1: 没有携带砖块
+        # 0 : 横向的长砖块
+        # 1 : 短砖块
+        # 2 : 厚砖块
+        # 3 : 纵向的长砖块
+        # 4 : 删除砖块
+
+        self.pressed = [False, False, False]
+
+        self.floor_number = 0
+
+        self.rotate = 0
+
+        self.is_upped = True
+
